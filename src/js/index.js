b5a764ec6743d49bfbefa089a16933e5b80ac71c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-19 21:01:38 +0100

    added an array with objects (img) for my baner

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,21 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
+const img = document.querySelector('.slider');
+const h1 = document.querySelector('h1');
+
+
+const slideList = [
+    {
+        img: "../assets/img/img1.jpg",
+        text: "pierwszy text",
+    },
+    {
+        img: "../assets/img/img2.jpg",
+        text: "drugi text",
+    },
+    {
+        img: "../assets/img/img3.jpg",
+        text: "trzeci text",
+    }
+]
